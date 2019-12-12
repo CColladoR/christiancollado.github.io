136040658aeb49a9537e117d7dbe89df3e6abd2b
CCR1O <ChristianColladoRuiz@gmail.com>
2019-12-12 17:32:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ var VERSION = 'version_01'              // Version of the off-line cache (change
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/christiancollado.github.io/',                     // If you have separate JS/CSS files,
-  '/christiancollado.github.io/index.html'            // add path to those files here
+  '/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
